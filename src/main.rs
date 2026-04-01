c2e930cfe820cb86af65233d926197563ff6a466
U-THINKPAD\ky0he1 <kyouta1218@gmail.com>
2026-04-01 18:38:01 +0900

    Implement Step 1.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
+use std::env;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+
+    if args.len() < 2 {
+        eprintln!("Usage: unwraped <file>");
+        std::process::exit(1);
+    }
+
+    let file_path = &args[1];
+    println!("File: {}", file_path);
 }
